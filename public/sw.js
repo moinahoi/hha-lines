171e33152ec41bb2599a03264acc8fa002fd474e
whocaresz <post2710@gmail.com>
2020-11-09 20:39:12 +0100

    cache icons

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,23 @@
 const cacheName = 'HHA-Lines';
-const assets = ['/', '/index.html', '/manifest.json', '/logo.png'];
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo.png',
+  '/icon/android-icon-192x192.png',
+  '/icon/apple-icon-180x180.png',
+  '/icon/apple-icon-152x152.png',
+  '/icon/apple-icon-144x144.png',
+  '/icon/apple-icon-120x120.png',
+  '/icon/apple-icon-114x114.png',
+  '/icon/favicon-96x96.png',
+  '/icon/apple-icon-76x76.png',
+  '/icon/apple-icon-72x72.png',
+  '/icon/apple-icon-60x60.png',
+  '/icon/apple-icon-57x57.png',
+  '/icon/favicon-32x32.png',
+  '/icon/favicon-16x16.png',
+];
 
 // Cache all the files to make a PWA
 self.addEventListener('install', (e) => {
