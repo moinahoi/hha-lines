833f90b5ef340fd70f6aa6471002142c2e5f467f
whocaresz <post2710@gmail.com>
2020-11-17 22:21:15 +0100

    update service worker, add cache on fetch for non pre-cached ressources

--- public/sw.js
+++ public/sw.js
@@ -1,45 +1,32 @@
-const cacheName = 'HHA-Lines';
-const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/logo.png',
-  '/icon/android-icon-192x192.png',
-  '/icon/apple-icon-180x180.png',
-  '/icon/apple-icon-152x152.png',
-  '/icon/apple-icon-144x144.png',
-  '/icon/apple-icon-120x120.png',
-  '/icon/apple-icon-114x114.png',
-  '/icon/favicon-96x96.png',
-  '/icon/apple-icon-76x76.png',
-  '/icon/apple-icon-72x72.png',
-  '/icon/apple-icon-60x60.png',
-  '/icon/apple-icon-57x57.png',
-  '/icon/favicon-32x32.png',
-  '/icon/favicon-16x16.png',
-];
+const cacheName = 'HHA-Linien';
+const cacheOnInstall = ['/', '/index.html', '/manifest.json', '/logo.png'];
 
-// Cache all the files to make a PWA
+// pre-cache PWA on install
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      // Our application only has two files here index.html and manifest.json
-      // but you can add more such as style.css as your app grows
-      return cache.addAll(assets);
+      return cache.addAll(cacheOnInstall);
     })
   );
 });
 
-// Our service worker will intercept all fetch requests
-// and check if we have cached the file
-// if so it will serve the cached file
+// listen to fetch
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches
-      .open(cacheName)
-      .then((cache) => cache.match(event.request, { ignoreSearch: true }))
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request, { ignoreSearch: true }).then((response) => {
+      if (response) {
+        return response;
+      }
+      return (
+        // fetch ressource
+        fetch(event.request).then((response) => {
+          // cache fetched ressource clone
+          return caches.open(cacheName).then((cache) => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        })
+      );
+    })
   );
 });
